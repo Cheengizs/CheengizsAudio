fcf51f06a8de1f0ce725cca2aeb92b88b793044f
Cheengizs <valinskiyartem@gmail.com>
2025-10-10 19:27:50 +0300

    added new music, you can play different music

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,41 +1,51 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() // allow all domains (for testing)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(); // enable CORS middleware
 
-var summaries = new[]
+
+List<Music> pl = new()
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Music() { Id = 1, Title = "Cupsize yura yura", Path = @"D:\dotnet_projs\CheengizsAudio\audios\cupsize.mp3" },
+    new Music() { Id = 2, Title = "Maks Corj", Path = @"D:\dotnet_projs\CheengizsAudio\audios\Макс Корж - 2 Типа Людей.mp3"}
 };
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapGet("/", async () => Results.Ok(pl));
+
+app.MapGet("/audio/{id}", async (HttpContext context , int id) =>
+{
+    var filePath = pl.Find(x => x.Id == id)?.Path;
+    // var filePath = context.Request;
+
+    context.Response.ContentType = "audio/mpeg"; // important for browsers
+
+    await context.Response.SendFileAsync(filePath);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class Music
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Path { get; set; }
 }
